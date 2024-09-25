15cccf44d39b4ab52acd5fed3da45a5bcae1e7cf
Maksim <121749232+MaksimCh1988@users.noreply.github.com>
2024-09-25 19:14:52 +0300

    first commit

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="bg-slate-200/20 absolute inset-0 backdrop-blur-sm flex items-center justify-center">
+      <div className="loader"></div>;
+    </div>
+  );
+};
+
+export default Loader;
